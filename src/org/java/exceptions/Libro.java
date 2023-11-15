6c294e5a78e62bde7e8f5757f2fcfb936e08077a
alep-2000 <alessiopiras056@gmail.com>
2023-11-15 15:17:18 +0100

    Creazione classi e relative eccezzioni

--- src/org/java/exceptions/Libro.java
+++ src/org/java/exceptions/Libro.java
@@ -1,5 +1,70 @@
 package org.java.exceptions;
 
 public class Libro {
+	private String titolo;
+	private int numeroPagine;
+	private String autore;
+	private String editore;
+	
+	public Libro(String titolo, int numeroPagine, String autore, String editore) throws Exception {
+		setTitolo(titolo);
+		setNumeroPagine(numeroPagine);
+		setAutore(autore);
+		setEditore(editore);
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) throws Exception {
+		
+		if (titolo == null || titolo.isEmpty() ) 
+			throw new Exception("Titolo è vuoto o nullo");
+		
+		this.titolo = titolo;
+	}
+
+	public int getNumeroPagine() {
+		return numeroPagine;
+	}
+
+	public void setNumeroPagine(int numeroPagine) throws Exception {
+		if (numeroPagine <= 0) 
+			throw new Exception("Il numero pagine ha un numero pagine inferiore a 0");
+		
+		this.numeroPagine = numeroPagine;
+	}
+
+	public String getAutore() {
+		return autore;
+	}
+
+	public void setAutore(String autore) throws Exception {
+		if (autore == null || autore.isEmpty() ) 
+			throw new Exception("Autore è vuoto o nullo");
+		
+		this.autore = autore;
+	}
+
+	public String getEditore() {
+		return editore;
+	}
+
+	public void setEditore(String editore) throws Exception {
+		if (editore == null || editore.isEmpty() ) 
+			throw new Exception("Editore è vuoto o nullo");
+		
+		this.editore = editore;
+	}
+	
+	@Override
+	public String toString() {
+		return "Libro \n"
+				+ "Titolo: " + getTitolo() + "\n"
+				+ "Numero Pagine: " + getNumeroPagine() + "\n"
+				+ "Autore: " + getAutore() + "\n"
+				+ "Editore: " + getEditore() + "\n"; 
+	}
 
 }
--- src/org/java/exceptions/Main.java
+++ src/org/java/exceptions/Main.java
@@ -1,8 +1,50 @@
 package org.java.exceptions;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws Exception {
 		
+		// EXCEPTION		
+		Scanner in = new Scanner(System.in);
 		
+		System.out.print("Quanti libri vuoi creare? ");
+		String strLibro = in.nextLine();
+		int intLibro = Integer.valueOf(strLibro);
+		
+		int libroIndex = 0;
+		Libro[] libri = new Libro[intLibro];
+		while(libroIndex < intLibro) {
+			
+			System.out.println("Libro " + (libroIndex + 1));
+			System.out.print("Titolo: ");
+			String titolo = in.nextLine();
+			
+			System.out.print("NumeroPagine: ");
+			String numeroPagine = in.nextLine();
+			int intNumeroPagine = Integer.valueOf(numeroPagine);
+			
+			System.out.print("Autore: ");
+			String autore = in.nextLine();
+			
+			System.out.print("Editore: ");
+			String editore = in.nextLine();
+			
+			Libro libro = null;
+			
+			try {
+				
+				libro = new Libro(titolo, intNumeroPagine, autore, editore);
+				
+				libri[libroIndex++] = libro;
+			} catch (Exception e) {
+				
+				System.out.println("Errore: " + e.getMessage());
+			}
+		}
+		
+		System.out.println(Arrays.asList(libri));
 	}
 }
